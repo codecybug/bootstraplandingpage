71041e2aa4a2936f663898d040a397c69fd52f51
Codecybug <basiruyekinni@gmail.com>
2021-04-08 08:22:52 -0700

    Simple Bootstrap Landing page

---
+++ index.php
@@ -0,0 +1,175 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Kolade">
+    <title>Accomodation Request · The home of secure and reliable property </title>
+
+    <link rel="canonical" href="https://acreq.com">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <link href="css/main.css" rel="stylesheet">
+    <link rel="stylesheet" href="fontawesome/css/all.min.css">
+  </head>
+  <body>
+    
+<header>
+  <nav class="navbar navbar-expand-md navbar-dark fixed-top">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#"><i class="fa fa-home"></i>AcReq</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarCollapse">
+        <ul class="navbar-nav me-auto mb-2 mb-md-0">
+          <li class="nav-item">
+            <a class="nav-link" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item ">
+            <a class="nav-link" aria-current="page" href="#">Register</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" aria-current="page" href="#">Login</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Dashboard</a>
+          </li>
+          
+        </ul>
+        <form class="d-flex">
+          <input class="form-control me-2" type="search" placeholder="Enter property name,location" aria-label="Search">
+          <button class="btn text-white" style="border-color: white;" type="submit">Search</button>
+        </form>
+      </div>
+    </div>
+  </nav>
+</header>
+
+<main>
+<div class="container-fluid" style="margin-top: 4rem;">
+  <div class="container">
+    <div class="jumbotron jumbotron-fluid">
+      <h1 class="display-4">Welcome to Accomodation Request Portal</h1>
+      <hr class="my-4">
+      <p>We help you get the best and secure accomodation both on campus and off-campus</p>
+      <p>We are group of agents working together we take responsibilty to do the needy for you as a student concerning accomodation</p>
+      <p class="lead">
+        <a href="" class="btn btn-primary property-link">Sign Up Now</a>
+      </p>
+    </div>
+  </div>
+  
+</div>
+  
+
+
+  
+
+  <div class="container" style="margin-top: 3rem; margin-bottom: 3rem;">
+
+    <div class="row g-3">
+      <div class="col-xl-3 col-lg-4 col-md-4">
+        <div class="card text-center shadow-sm">
+          <h6 class="card-header">Price: &#8358;2,000,000</h6>
+          <img src="uploads/tade.jpg" class="card-img-top">
+          <div class="card-body">
+            <h5 class="card-title">A three bedrooms flat</h5>
+            <p class="card-text">some quick text about the property in the listing</p>
+            <p class="card-text text-muted">Location: Mayfair Osun State</p>
+            <a href="" class="btn btn-primary property-link text-white">View details &raquo;</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-xl-3 col-lg-4 col-md-4">
+        <div class="card text-center shadow-sm">
+          <h6 class="card-header">Price: &#8358;2,000,000</h6>
+          <img src="uploads/tade.jpg" alt="" class="card-img-top img-fluid">
+          <div class="card-body">
+            <h5 class="card-title">A three bedrooms flat</h5>
+            <p class="card-text">some quick text about the property in the listing</p>
+            <p class="card-text text-muted">Location: Mayfair Osun State</p>
+            <a href="" class="btn btn-primary property-link text-white">View details &raquo;</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-xl-3 col-lg-4 col-md-4">
+        <div class="card text-center shadow-sm">
+          <h6 class="card-header">Price: &#8358;2,000,000</h6>
+          <img src="uploads/tade.jpg" alt="" class="card-img-top img-fluid">
+          <div class="card-body">
+            <h5 class="card-title">A three bedrooms flat</h5>
+            <p class="card-text">some quick text about the property in the listing</p>
+            <p class="card-text text-muted">Location: Mayfair Osun State</p>
+            <a href="" class="btn btn-primary property-link text-white">View details &raquo;</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-xl-3 col-lg-4 col-md-4 col-sm-12">
+        <div class="card text-center shadow-sm">
+          <h6 class="card-header">Price: &#8358;2,000,000</h6>
+          <img src="uploads/tade.jpg" alt="" class="card-img-top img-fluid">
+          <div class="card-body">
+            <h5 class="card-title">A three bedrooms flat</h5>
+            <p class="card-text">some quick text about the property in the listing</p>
+            <p class="card-text text-muted">Location: Mayfair Osun State</p>
+            <a href="" class="btn btn-primary property-link text-white">View details &raquo;</a>
+          </div>
+        </div>
+      </div>
+
+    </div>
+
+  </div>
+
+
+  <!-- FOOTER -->
+  <footer class="container-fluid bg-dark text-white">
+    <div class="container footer-div">
+      <div class="row">
+        <div class="col-lg-3 col-md-6 col-sm-12 my-2">
+          <h4 class="mb-3 section-title">Quick links</h4>
+            <a href="">Become our agent</a>
+            <a href="">About Us</a>
+            <a href="">Privacy Policy</a>
+        </div>
+
+        <div class="col-lg-3  col-md-6 col-sm-12 my-2">
+          <h4 class="mb-3 section-title">Our location </h4>
+            <address>
+              B 147 Mayfair Ile-ife,
+              <br> Osun State
+              Nigeria
+            </address>
+            <p>Tel: 09038639636,08132961732</p>
+        </div>
+
+        <div class="col-lg-3 col-md-6 col-sm-12 my-2 ">
+          <h4 class="mb-3 section-title">Social links</h4>
+            <a href="">Facebook</a>
+            <a href="">Whatsapp</a>
+            <a href="">Instagram</a>
+        </div>
+
+        <div class="col-lg-3 col-md-6 col-sm-12 my-2">
+          <h4 class="mb-3 section-title">Contacts </h4>
+            <p>Tel: 09038639636,08132961732</p>
+            <p>Our Hotlines: +2348132961732, +2349038639636</p>
+        </div>
+
+      </div>  
+    </div>
+  </footer>
+</main>
+
+    <script src="js/jquery.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    
+      
+  </body>
+</html>
